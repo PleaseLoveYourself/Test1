810b24f5123300ba32078c314b3f166a12fc7f5a
PleaseLoveYourself <49891259+PleaseLoveYourself@users.noreply.github.com>
2019-04-23 11:31:27 +0800

    Add files via upload

---
+++ Demo01.java
@@ -0,0 +1,15 @@
+package Day3;
+
+public class Demo01 {
+    public static void main(String[] args){
+        int a=-10;
+        int b=3;
+        System.out.println(b-a);
+        int c=5,d=5;
+        System.out.println(c+d);
+        int e=4;
+        e+=8;
+        System.out.println(e);
+
+    }
+}
